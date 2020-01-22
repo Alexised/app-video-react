58d232cd4101d1e13da88e9a23e2551e8591e75f
alexis <edwinalexis1996@gmail.com>
2020-01-22 10:37:30 -0500

    ident

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import '../assets/styles/App.scss'
 
-const HelloWorld =()=>(
-    <h1>Hola Mundo</h1>
+const HelloWorld = () => (
+  <h1>Hola Mundo</h1>
 );
 
 export default HelloWorld;
